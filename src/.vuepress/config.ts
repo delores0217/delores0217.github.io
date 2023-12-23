48eeba6f16d36b906f264cff0f2c6744e99a654a
Yalan Wen <delores0217@YalandeMacBook-Pro.local>
2023-12-23 22:27:19 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/delores0217.github.io/",
 
   lang: "zh-CN",
   title: "Delores' Castle",
